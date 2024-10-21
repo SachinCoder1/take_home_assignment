c735db7b23033ede427da74df4cef6afb5affe6a
Sachin Gurjar <sachinhlo232@gmail.com>
2024-10-21 19:38:09 +0530

    cloudinary

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="flex items-center justify-center w-full h-screen bg-black">
       <div className="bg-[#37393F] md:w-[1082px] md:h-[668px] px-4 md:pb-16 pt-4 rounded-xl">
         <h2 className='pb-4 text-xl text-white'>Cropper</h2>
-        <Main videoSrc={"https://take-home-assignment-two.vercel.app/football_match.mp4"} />
+        <Main videoSrc={"https://res.cloudinary.com/ddhv43zka/video/upload/v1729519568/football_match.mp4"} />
       </div>
     </div>
   );
