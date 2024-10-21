b6dc622cfaeeecbf1ebc329122c0e172ae03821e
Sachin Gurjar <sachinhlo232@gmail.com>
2024-10-21 18:53:50 +0530

    path fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="flex items-center justify-center w-full h-screen bg-black">
       <div className="bg-[#37393F] md:w-[1082px] md:h-[668px] px-4 md:pb-16 pt-4 rounded-xl">
         <h2 className='pb-4 text-xl text-white'>Cropper</h2>
-        <Main videoSrc={"/football_match1.mp4"} />
+        <Main videoSrc={"/football_match.mp4"} />
       </div>
     </div>
   );
